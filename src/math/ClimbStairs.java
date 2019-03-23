1bac7e014d1d4d1629f6d0a0b365ef4e9c082a6a
sandepatel <sandepatel@ebay.com>
2019-03-23 22:49:01 +1100

    Added algo + maths problems

---
+++ src/math/ClimbStairs.java
@@ -0,0 +1,72 @@
+package math;
+
+/**
+ You are climbing a stair case. It takes n steps to reach to the top.
+
+ Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ Note: Given n will be a positive integer.
+
+ Example 1:
+
+ Input: 2
+ Output: 2
+ Explanation: There are two ways to climb to the top.
+ 1. 1 step + 1 step
+ 2. 2 steps
+ Example 2:
+
+ Input: 3
+ Output: 3
+ Explanation: There are three ways to climb to the top.
+ 1. 1 step + 1 step + 1 step
+ 2. 1 step + 2 steps
+ 3. 2 steps + 1 step
+
+ Input = 4
+ Output = 5
+ 1,1,1,1
+ 1,1,2
+ 1,2,1
+ 2,1,1
+ 2,2
+
+ Input 5
+ Output = 8
+
+
+ 1,2,3,5,8,
+
+
+ Looks like fibonacci :
+ */
+class ClimbStairs {
+
+    public static void main(String[] args) {
+        int n = 1;
+        int res = new ClimbStairs().climbStairs(n);
+        System.out.println("Output for " + n + " = " + res);
+
+    }
+
+
+    public int climbStairs(int n) {
+
+        if(n<=1) {
+            return 1;
+        }
+
+        int n1;
+        int n2 = 1;
+        int sum = 0;
+        while (n >= 0) {
+            n1 = sum;
+            sum = n1+n2;
+            n2 = n1;
+            n--;
+
+        }
+
+        return sum;
+    }
+}
